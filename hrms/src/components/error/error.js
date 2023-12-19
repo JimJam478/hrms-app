92d5fdbc886c79be24bd70f1cf7c1ce6b7a1dc8a
JimJam47 <allensreebinoj2010@gmail.com>
2023-12-19 20:17:15 +0530

    styles error

--- hrms/src/components/error/error.js
+++ hrms/src/components/error/error.js
@@ -1,9 +1,9 @@
 
 export default function ErrorPage() {
     return(
-        <div>
-        <h1 style={{color: "red",paddingTop: "400px"}}>ERROR 404: Employee ID not found</h1>
-
+        <div style={{paddingTop: "350px"}}>
+        <h1 style={{color: "red"}}>ERROR 404:</h1><br/>
+        <h2 style={{color: "aliceblue"}}>  Employee ID not found </h2>
         </div>
     )
 }
