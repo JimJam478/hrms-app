430dd1d6fad2f2b9e06f89a1f1fcab3422444858
JimJam47 <allensreebinoj2010@gmail.com>
2023-12-19 15:02:36 +0530

    Adds route for error page

--- hrms/src/App.js
+++ hrms/src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/navbar';
 import Home from './components/home/home';
-import Employees from './components/leaveform/leaveform';
 import EmployeeDetails from './components/employeedetails/employeedetails';
 import About from './components/about/about';
 import { Navigate } from "react-router-dom";
 import { EmployeeList2 } from './components/employeelist/employeelist';
+import ErrorPage from './components/error/error';
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/employees" element={<EmployeeList2/>} /> 
       <Route path="/about" element={<About />} />
+      <Route path="/error" element={<ErrorPage />} />
       <Route path="*" element={ <Navigate to="/" /> } />
       <Route path="/employees/:id" element={<EmployeeDetails />}
           render={({ params }) => <EmployeeDetails id={params.id} />}
