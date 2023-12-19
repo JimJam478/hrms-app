e72cba653c5c7016ae9b723bf276cd2ad687874c
JimJam47 <allensreebinoj2010@gmail.com>
2023-12-19 22:19:11 +0530

    stylizes error page

--- hrms/src/components/error/error.js
+++ hrms/src/components/error/error.js
@@ -1,7 +1,8 @@
+import './error.css'
 
 export default function ErrorPage() {
     return(
-        <div style={{paddingTop: "350px"}}>
+        <div id="error" className="container-fluid">
         <h1 style={{color: "red"}}>ERROR 404:</h1><br/>
         <h2 style={{color: "aliceblue"}}>  Employee ID not found </h2>
         </div>
