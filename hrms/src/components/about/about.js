240a4b4a070d75a8bc2ad95c5d09b06be34c8036
JimJam47 <allensreebinoj2010@gmail.com>
2023-12-19 22:18:41 +0530

    adds about page

--- hrms/src/components/about/about.js
+++ hrms/src/components/about/about.js
@@ -2,8 +2,16 @@ import "./about.css";
 
 export default function About() {
     return (
-        <div className="container-fluid">
-    <h1 id="header_about"> About page </h1>
-    </div>
+        <div id="about-container" className="container">
+          <h1 id="about">About Us</h1>
+          <p id="about">Welcome to my website! This is a toy HR Management System web application.</p>
+      
+          <h2 id="about"> What can you do ?</h2>
+          <p id="about">View employee list, details, leaves and even add leaves !</p>
+
+          <h2 id="about">Thanks ! </h2>
+          <p id="about"> May the force be with you. </p>
+        
+        </div>
   );
 } 
